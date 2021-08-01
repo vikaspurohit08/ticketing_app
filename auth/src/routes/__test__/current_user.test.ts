a9391811679ec9561cf3dc616b0db518651609bd
Vikas Purohit <vikas.purohit@dkatalis.com>
2021-08-01 15:33:29 +0530

    add global function of signin and update current user test

--- auth/src/routes/__test__/current_user.test.ts
+++ auth/src/routes/__test__/current_user.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about current user", async () => {
-  const data = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = data.get("Set-Cookie");
+  const cookie = await signin(); //using global function
   const response = await request(app)
     .get("/api/users/currentuser")
     .set("Cookie", cookie)
@@ -19,4 +11,13 @@ it("responds with details about current user", async () => {
   expect(response.body.currentUser.email).toEqual("test@test.com");
   //supertest doesn't handle the cookie created after signup and pass it while currentuser
   // so we manually capture the cookie and send to second request
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app)
+    .get("/api/users/currentuser")
+    .send()
+    .expect(200);
+
+  expect(response.body.currentUser).toEqual(null);
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,6 +1,11 @@
+import request from "supertest";
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import { app } from "../app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -25,3 +30,15 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "password";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  return response.get("Set-Cookie");
+};
