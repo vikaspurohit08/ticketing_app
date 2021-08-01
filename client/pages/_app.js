d419a01c014f32db508dc8c9fe987374b5ef3ef3
Vikas Purohit <vikas.purohit@dkatalis.com>
2021-08-01 20:43:28 +0530

    setup bootstrap for the app. And add signup form

---
+++ client/pages/_app.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
+const setCSS = ({ Component, pageProps }) => {
+    return <Component {...pageProps} />
+}
+
+export default setCSS;
+
+//used to import global css
+
