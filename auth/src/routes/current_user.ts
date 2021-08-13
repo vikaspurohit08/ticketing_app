9fae67281a476759cce97336a65a6a96f4dd2730
Vikas Purohit <vikaspurohit08@gmail.com>
2021-08-13 17:34:21 +0530

    make a common npm package and use in all services

--- auth/src/routes/current_user.ts
+++ auth/src/routes/current_user.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { currentUser } from "../middlewares/current-user";
+import { currentUser } from "@vpticketsapp/common";
 
 const router = express.Router();
 
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,10 +1,9 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { User } from "../models/user";
-import { validateRequest } from "../middlewares/validate-request";
-import { BadRequestError } from "../errors/bad-request.error";
 import { Password } from "../services/password";
 import jwt from "jsonwebtoken";
+import { validateRequest, BadRequestError } from "@vpticketsapp/common";
 
 const router = express.Router();
 
