73f27fb952bf5eb553420b53f7e88a8d47b17e70
Vikas Purohit <vikaspurohit08@gmail.com>
2021-09-08 19:00:39 +0530

    add basic order routes

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -5,10 +5,10 @@ import "express-async-errors"; // used since check app.all method.
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@vpticketsapp/common";
-import { createTicketRouter } from "./routes/new";
-import { showTicketRouter } from "./routes/show";
-import { indexTicketRouter } from "./routes";
-import { updateTicketRouter } from "./routes/update";
+import { indexOrderRouter } from "./routes";
+import { newOrderRouter } from "./routes/new";
+import { showOrderRouter } from "./routes/show";
+import { deleteOrderRouter } from "./routes/delete";
 
 const app = express();
 app.set("trust proxy", true);
@@ -21,10 +21,10 @@ app.use(
 );
 app.use(currentUser);
 
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+app.use(deleteOrderRouter);
 
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
