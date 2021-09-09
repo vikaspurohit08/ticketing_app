e258d3249e0609235d7dba005e653c4f13bc5925
Vikas Purohit <vikaspurohit08@gmail.com>
2021-09-09 16:58:56 +0530

    add cancel order api with test

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,28 @@
+import {
+  NotAuthorizedError,
+  NotFoundError,
+  OrderStatus,
+} from "@vpticketsapp/common";
 import express, { Request, Response } from "express";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
+  const { orderId } = req.params;
+  const order = await Order.findById(orderId).populate("ticket");
+  if (!order) {
+    throw new NotFoundError();
+  }
+  if (order.userId !== req.currentUser!.id) {
+    throw new NotAuthorizedError();
+  }
+  order.status = OrderStatus.Cancelled;
+  await order.save();
+
+  //publish event that order was cancelled
+
+  res.status(204).send(order);
 });
 
 export { router as deleteOrderRouter };
