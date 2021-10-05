0dedbb188de6232499cca3e96b8377b1b6988b3b
Vikas Purohit <vikaspurohit08@gmail.com>
2021-10-05 21:18:17 +0530

    add delete order test

--- orders/src/routes/__test__/delete.spec.ts
+++ orders/src/routes/__test__/delete.spec.ts
@@ -3,6 +3,7 @@ import { Ticket } from "../../models/ticket";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@vpticketsapp/common";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   //create a ticket with ticket model
@@ -33,4 +34,32 @@ it("marks an order as cancelled", async () => {
   expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
 });
 
-it.todo("emits event when order is cancelled", async () => {});
+it("emits event when order is cancelled", async () => {
+  //create a ticket with ticket model
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+  await ticket.save();
+
+  const user = global.signin();
+  // make request to create an order
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  // make request to cancel the order
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  // expect to make sure order is cancelled
+  const updatedOrder = await Order.findById(order.id);
+
+  expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled);
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
