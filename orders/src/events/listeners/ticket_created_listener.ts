b9ccdc571b65fd9fd24e4ceeb6711812cf7e7d8f
Vikas Purohit <vikaspurohit08@gmail.com>
2021-10-11 20:22:31 +0530

    add ticket created listener with queu group name file and ticket build changes

---
+++ orders/src/events/listeners/ticket_created_listener.ts
@@ -0,0 +1,17 @@
+import { Message } from "node-nats-streaming";
+import { Subjects, Listener, TicketCreatedEvent } from "@vpticketsapp/common";
+import { queueGroupName } from "./queue_group_name";
+import { Ticket } from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  subject: Subjects.TicketCreated = Subjects.TicketCreated;
+  queueGroupName: string = queueGroupName;
+
+  async onMessage(data: TicketCreatedEvent["data"], msg: Message) {
+    const { id, title, price } = data;
+    const ticket = Ticket.build({ id, title, price });
+    await ticket.save();
+
+    msg.ack();
+  }
+}
