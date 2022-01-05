fbbc74c76f7e1fbf39a1f7f10f2f898d0084aa8b
Vikas Purohit <vikaspurohit08@gmail.com>
2022-01-05 19:25:48 +0530

    extract the versioning function as common

--- orders/src/events/listeners/ticket_updated_listener.ts
+++ orders/src/events/listeners/ticket_updated_listener.ts
@@ -7,11 +7,7 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName: string = queueGroupName;
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const ticket = await Ticket.findOne({
-      _id: data.id,
-      version: data.version - 1, //which means the existing ticket version
-      // in db should be just 1 less than the version received from listener
-    });
+    const ticket = await Ticket.findByEvent(data);
 
     if (!ticket) {
       throw new Error("Ticket not found");
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -18,6 +18,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -44,7 +48,13 @@ const ticketSchema = new mongoose.Schema(
 
 ticketSchema.set("versionKey", "version");
 ticketSchema.plugin(updateIfCurrentPlugin);
-
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
+  return Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1, //which means the existing ticket version
+    // in db should be just 1 less than the version received from listener
+  });
+};
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
     _id: attrs.id,
