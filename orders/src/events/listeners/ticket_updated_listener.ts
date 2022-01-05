5c0a4a61e9c770fa8f482ecceae94f2be6f5a6a2
Vikas Purohit <vikaspurohit08@gmail.com>
2022-01-05 19:38:13 +0530

    revert to previous versioning approach

--- orders/src/events/listeners/ticket_updated_listener.ts
+++ orders/src/events/listeners/ticket_updated_listener.ts
@@ -13,10 +13,10 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
       throw new Error("Ticket not found");
     }
 
-    const { title, price, version } = data; //If we don't want to couple the versioning with `mongoose-update-if-current` module
+    const { title, price } = data; //If we don't want to couple the versioning with `mongoose-update-if-current` module
     //then we can extract version from here and set it directly. Bcz we cannot be sure if upcoming version is in number format or time format or etc.
     //but for that we need to add multiple criterias to find the ticket refer ticket.ts change
-    ticket.set({ title, price, version });
+    ticket.set({ title, price });
     await ticket.save();
 
     msg.ack();
--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,6 +1,6 @@
 import { OrderStatus } from "@vpticketsapp/common";
 import mongoose from "mongoose";
-// import { updateIfCurrentPlugin } from "mongoose-update-if-current";
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import { Order } from "./order";
 
 interface TicketAttrs {
@@ -47,14 +47,7 @@ const ticketSchema = new mongoose.Schema(
 );
 
 ticketSchema.set("versionKey", "version");
-// ticketSchema.plugin(updateIfCurrentPlugin);
-ticketSchema.pre("save", function (done) {
-  this.$where = {
-    version: this.get("version") - 1, //update as per version criteria
-  };
-
-  done();
-});
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.findByEvent = (event: { id: string; version: number }) => {
   return Ticket.findOne({
