b9d6fd162c40f6188d1cf0def5034561277a8b4f
Vikas Purohit <vikaspurohit08@gmail.com>
2022-01-05 19:16:53 +0530

    add version control for tickets

--- orders/src/events/listeners/ticket_updated_listener.ts
+++ orders/src/events/listeners/ticket_updated_listener.ts
@@ -7,7 +7,11 @@ export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
   subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
   queueGroupName: string = queueGroupName;
   async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
-    const ticket = await Ticket.findById(data.id);
+    const ticket = await Ticket.findOne({
+      _id: data.id,
+      version: data.version - 1, //which means the existing ticket version
+      // in db should be just 1 less than the version received from listener
+    });
 
     if (!ticket) {
       throw new Error("Ticket not found");
